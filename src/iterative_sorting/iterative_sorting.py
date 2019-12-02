c177407ed6e2d5fe3f40bf447778855329bca17a
brettmadrid <brettmadrid9@gmail.com>
2019-12-01 16:15:02 -0800

    completed bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,12 +17,23 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
-# def bubble_sort(arr):
-#     return arr
+def bubble_sort(arr):
+    # set variable to track if swaps were made
+    swaps_occurred = True  # to start the loop
+    while swaps_occurred:
+        # Now reset swaps to False & only set to True if a swap occurs
+        swaps_occurred = False
+        for i in range(0, len(arr) - 1):
+            # if element to the right is smaller, swap them
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swaps_occurred = True
+
+    return arr
 
 
 # STRETCH: implement the Count Sort function below
 # def count_sort(arr, maximum=-1):
 #     return arr
 
-print(selection_sort([25, 67, 4, 33, 19, 40]))
+print(bubble_sort([25, 67, 4, 33, 19, 40]))
